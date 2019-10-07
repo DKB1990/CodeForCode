0ce3a805ff07215dd844e8b60f08809978eefb91
Dheeraj Bansal <Dheeraj.bansal14@gmail.com>
2019-10-07 16:17:00 +0400

    Create HighestNumberOfSquareRoot.cs

---
+++ HighestNumberOfSquareRoot.cs
@@ -0,0 +1,34 @@
+#region Find Highest Number of Square Roots
+public int FindDeepRoot(int startIndex, int endIndex)
+{
+    int count;
+    Dictionary<int, int> deepCount = new Dictionary<int, int>();
+    while (startIndex <= endIndex)
+    {
+        count = 1;
+        if (Math.Sqrt(startIndex) % 1 == 0)
+            deepCount.Add(startIndex, DeepRootCount(count, startIndex) - 1);
+        startIndex += 1;
+    }
+
+    return deepCount.Max(x => x.Value);
+}
+
+public int DeepRootCount(int count, int number)
+{
+    int newNum = number;
+    if (newNum % 1 == 0 && newNum > 1)
+    {
+        newNum = (int)Math.Sqrt(number);
+        count += DeepRootCount(count, newNum);
+    }
+
+    return count;
+}
+#endregion
+
+public static void Main()
+{
+    FindDeepRoot (20);
+    //output: 3 (16 has 2)
+}
